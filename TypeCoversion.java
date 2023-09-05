b9e3942925a143a82ea4e13938de0a27e97ab166
최예름 <101881124+fusiontwo@users.noreply.github.com>
2023-09-05 12:50:30 +0900

    Add files via upload

---
+++ TypeCoversion.java
@@ -0,0 +1,15 @@
+package week02.example01;
+
+public class TypeCoversion {
+
+	public static void main(String[] args) {
+		
+		byte b = 127;
+		int i = 100;
+
+		System.out.println(b+i);
+		System.out.println();
+		//
+	}
+
+}
